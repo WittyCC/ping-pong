f62a561b6e0b545278c855d227977f4ffc82bfe7
Witty Chang <wcc1213@gmail.com>
2017-06-15 13:31:23 -0700

    Modify scripts.js to separate user interface logic and place business logic in a separate function to call on in front end.

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,37 @@
+//business logic
+var userInput = parseInt($("input#numberInput").val());
+var countArray = [];
+var output;
+
+function countPingPong(userInput) {
+  if (userInput > 0) {
+    for (var i = 1; i <= userInput; i++) {
+      output = '';
+      if (i % 3 === 0 && i % 15 !== 0) {
+        output += "PING!";
+      } else if (i % 5 === 0 && i % 15 !== 0) {
+        output += "PONG!";
+      } else if (i % 3 === 0 && i % 5 ===0 && i % 15 === 0) {
+         output += "PING-PONG!";
+      } else if (output === '') {
+        output +=i;
+      }
+      countArray.push(output);
+    }
+  } else {
+    alert("Please enter a number greater than 0.");
+  }
+}
+
 //user interface logic
 $(document).ready(function() {
   $("form#userNumber").submit(function(event) {
     event.preventDefault();
     $("#numberInput").empty();
-
-    var userInput = parseInt($("input#numberInput").val());
-    var countArray = [];
-    var output;
-
-//business logic
-    if (userInput > 0) {
-      for (var i = 1; i <= userInput; i++) {
-        output = '';
-        if (i % 3 === 0 && i % 15 !== 0) {
-          output += "PING!";
-        }
-        if (i % 5 === 0 && i % 15 !== 0) {
-          output += "PONG!";
-        }
-        if (i % 3 === 0 && i % 5 ===0 && i % 15 === 0) {
-           output += "PING-PONG!";
-        }
-        if (output === '') {
-          output +=i;
-        }
-        countArray.push(output);
-      }
-    } else {
-      alert("Please enter a number greater than 0.");
-    }
-
     $("#countOutput").empty();
+
+    countPingPong(userInput);
+
     countArray.forEach(function(output) {
       $("#countOutput").append("<li>" + output + "</li>");
     });
